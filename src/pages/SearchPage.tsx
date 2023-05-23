a66084d5b551186dce1f5782a6d3edb76c4d98d1
Matuma1ni <nadejdav90@gmail.com>
2023-05-23 18:30:21 +0300

    add search page placeholder

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,5 +1,33 @@
+import { Button, IconButton } from '@mui/material'
+import './SearchPage.css'
+import { DisplaySettingsOutlined } from '@mui/icons-material'
+
+
 export const SearchPage = () => {
     return (
-        <div></div>
+        <div className="searchPageLayout">
+            <div className="searchPageHeader">
+                <input placeholder='Enter search value' className="searchInput"></input>
+                <Button sx={{
+                    margin: "0px 13px 0px 13px",
+                    width: "179px",
+                    height: "40px",
+                    background: "rgba(60, 134, 244, 0.2)",
+                    borderRadius: "8px",
+                    color: "#3C86F4",
+                }}>Search</Button>
+                <IconButton sx={{
+                    display: "inline-block",
+                    width: "40px",
+                    height: "40px",
+                    background: "rgba(60, 134, 244, 0.2)",
+                    borderRadius: "8px",
+                    color: "#3C86F4",
+                }}>
+                    <DisplaySettingsOutlined />
+                </IconButton>
+            </div>
+            <div className="searchPagePlaceHolder">No data to display<br />Please start search to display results</div>
+        </div>
     )
 }
