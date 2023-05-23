9cb9f91fda0e66f76dab6b3f3c74bddfae71d04f
Matuma1ni <nadejdav90@gmail.com>
2023-05-23 18:30:21 +0300

    add search page placeholder

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,24 @@
 import { AppBar, Box, Button, Stack, Toolbar } from "@mui/material"
 import { auth, logout } from "../authConfig"
-import { useAuthState } from "react-firebase-hooks/auth";
+import { useAuthState } from "react-firebase-hooks/auth"
+import './Header.css'
 
 export const Header = () => {
     //const [user, loading, error] = useAuthState(auth);
     const [user] = useAuthState(auth);
-    
+
     return (
-        <AppBar position="fixed" sx={{color: "black", backgroundColor: 'white', borderBottom: "1px solid #D8E7FF", boxShadow: "0px 0px 0px 0px"}}>
-            <Toolbar variant="dense" > 
+        <AppBar position="fixed" sx={{
+            color: "black",
+            backgroundColor: 'white',
+            borderBottom: "1px solid #D8E7FF",
+            boxShadow: "0px 0px 0px 0px"
+        }}>
+            <Toolbar variant="dense" >
                 <Box sx={{ flexGrow: 1 }} />
-                <Stack direction="row" spacing={2} >
+                <Stack direction="row" spacing={2} sx={{marginRight: "7%"}}>
                     <p>{user?.email}</p>
-                    <Button onClick={logout} sx={{color: 'grey', backgroundColor: 'white'}}>Log out</Button>
+                    <Button onClick={logout} sx={{ color: 'grey', backgroundColor: 'white' }}>Log out</Button>
                 </Stack>
             </Toolbar>
         </AppBar>
